1c9f9d3dcfd48b0bd4a560f678bb7bcd3fe44856
Polun <polunwu@gmail.com>
2020-04-05 22:38:14 +0800

    add Chat.vue & passing props

---
+++ src/components/Chat.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="chat container">
+    <h2>Chat {{this.name}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Chat",
+  props: ["name"],
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+}
+.welcome h2 {
+  font-size: 3em;
+}
+
+.welcome button {
+  margin: 30px auto;
+}
+</style>
