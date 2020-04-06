3ddc4dafab87ce57e74f86cd72b4bb3c636205e9
Polun <polunwu@gmail.com>
2020-04-06 18:17:32 +0800

    add NewMessage Component

---
+++ src/components/NewMessage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="new-message">
+    <form @submit.prevent="addMessage">
+      <label for="new-message">New Message (enter to add)</label>
+      <input type="text" name="new-message" v-model="newMessage" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewMessage",
+  props: ["name"],
+  data() {
+    return {
+      newMessage: null
+    };
+  },
+  methods: {
+    addMessage() {
+      console.log(this.name, this.newMessage, Date.now());
+      this.newMessage = null;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
